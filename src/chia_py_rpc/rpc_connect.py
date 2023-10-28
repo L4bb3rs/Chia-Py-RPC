488eea247adf00f227e19c62b922b0637267737a
Dan <dan@adcyber.co.uk>
2023-10-28 23:30:42 +0100

    Update rpc_connect.py

--- src/chia_py_rpc/rpc_connect.py
+++ src/chia_py_rpc/rpc_connect.py
@@ -1,61 +1,81 @@
-import os
 import json
 import requests
 import urllib3
-from typing import Optional, Tuple, Dict, Any
+import os
+
+from typing import Optional, Tuple
+
 
 class WalletRPCConnect:
     """
-    A class for connecting to a Chia Wallet RPC service.
+    This module provides a class for connecting to a Chia Wallet RPC service.
+
+    The `WalletRPCConnect` class provides methods for submitting Chia RPC calls and
+    receiving JSON responses.
+
+    Usage:
+        >>> wallet_rpc = WalletRPCConnect(url='https://localhost:9256/',
+                                        cert=('path/to/cert', 'path/to/key'))
+        >>> response = wallet_rpc.submit('get_wallet_balance', '{}')
+        >>> print(response)
 
     Attributes:
         url (str): URL of the Chia RPC service.
-        cert (Tuple[str, str]): Paths to the SSL certificate and private key files.
+        cert (tuple): Tuple containing paths to the SSL certificate and private key files.
+
+    Methods:
+        submit(chia_call: str, data: str) -> str:
+            Submit a Chia RPC call to the specified URL with the given data.
+
+            Args:
+                chia_call (str): Name of the Chia RPC call to be made.
+                data (str): Data to be sent in the request.
+
+            Returns:
+                str: JSON response as a string with indentation and sorted keys.
     """
 
-    DEFAULT_URL = "https://localhost:9256/"
-    DEFAULT_CERT = (
-        os.path.expanduser('~/.chia/mainnet/config/ssl/full_node/private_full_node.crt'),
-        os.path.expanduser('~/.chia/mainnet/config/ssl/full_node/private_full_node.key')
-    )
 
-    def __init__(self, url: Optional[str] = None, cert: Optional[Tuple[str, str]] = None, disable_warnings: bool = False) -> None:
+    def __init__(self, url: Optional[str] = None,
+                cert: Optional[Tuple[str, str]] = None):
         """
-        Initialize the WalletRPCConnect instance.
+        Initialize ChiaRPC instance with the provided URL and certificate.
 
         Args:
-            url (Optional[str]): URL of the Chia RPC service.
-            cert (Optional[Tuple[str, str]]): Paths to the SSL certificate and private key files.
-            disable_warnings (bool, optional): Disable SSL warnings. Defaults to False.
+            url (str, optional): URL of the Chia RPC service. Defaults to None.
+            cert (tuple, optional): Tuple containing paths to the SSL certificate and private key files. Defaults to None.
         """
-        self.url = url or self.DEFAULT_URL
-        self.cert = cert or self.DEFAULT_CERT
+        default_url = "https://localhost:9256/"
+        default_cert = (
+            os.path.expanduser(
+                '~/.chia/mainnet/config/ssl/full_node/private_full_node.crt'),
+            os.path.expanduser(
+                '~/.chia/mainnet/config/ssl/full_node/private_full_node.key')
+        )
+
+        self.url = url or default_url
+        self.cert = cert or default_cert
         self.headers = {"Content-Type": "application/json"}
+        urllib3.disable_warnings()
 
-        if disable_warnings:
-            urllib3.disable_warnings()
 
-    def submit(self, chia_call: str, data: str) -> Dict[str, Any]:
+    def submit(self, chia_call: str, data: str):
         """
-        Submit a Chia RPC call.
+        Submit data to the specified URL.
 
         Args:
-            chia_call (str): The Chia RPC call to be made.
-            data (str): The data to be sent in the request.
+            chia_call (str): Chia RPC call to be made.
+            data (str): Data to be sent in the request.
 
         Returns:
-            Dict[str, Any]: The JSON response.
+            str: JSON response as a string with indentation and sorted keys.
         """
-        try:
-            response = requests.post(
-                f"{self.url}{chia_call}",
-                data=data,
-                headers=self.headers,
-                cert=self.cert,
-                verify=True  # Enable SSL verification
-            )
-            response.raise_for_status()  # Raise HTTPError for bad responses
-            return response.json()
-        except requests.RequestException as e:
-            print(f"An error occurred: {e}")
-            return {}
+        response = requests.post(
+            self.url + chia_call,
+            data=data,
+            headers=self.headers,
+            cert=self.cert,
+            verify=False)
+        response_text = response.text
+        response_json = json.loads(response_text)
+        return json.dumps(response_json, indent=4, sort_keys=True)
